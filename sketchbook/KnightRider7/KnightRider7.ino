f1b6db7e1564514affdbbd88d143f5f7ca04bf95
Dracos <marty.vance@gmail.com>
2013-10-07 23:02:41 -0600

    Update LED pins to avoid timer conflicts

--- sketchbook/KnightRider7/KnightRider7.ino
+++ sketchbook/KnightRider7/KnightRider7.ino
@@ -23,7 +23,8 @@ Board options =========================================================
 const uint8_t num_leds = 10;
 
 // declare a pin for each LED
-uint8_t pinArray[num_leds] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+// pins 9,10,11 cause conflicts with the timers
+uint8_t pinArray[num_leds] = {2, 3, 4, 5, 6, 7, 8, 44, 45, 46};
 
 // initialize brightness for each LED
 int bright[num_leds] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
